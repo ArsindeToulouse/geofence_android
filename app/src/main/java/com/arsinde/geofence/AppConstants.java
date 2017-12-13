b972700f235950e010d3c52a7ccb655f41c46f05
Yulia <mobile@traft.ru>
2017-12-13 16:01:24 +0300

    First commit

---
+++ app/src/main/java/com/arsinde/geofence/AppConstants.java
@@ -0,0 +1,9 @@
+package com.arsinde.geofence;
+
+class AppConstants {
+
+    private AppConstants() {}
+
+    static final int LOCATION_PERMANENT_REQUEST_CODE = 1;
+    static final int GEOFENCE_RADIUS_IN_METERS = 300;
+}
---
+++ app/src/main/java/com/arsinde/geofence/LocationAlertActivity.java
@@ -0,0 +1,194 @@
+package com.arsinde.geofence;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class LocationAlertActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = LocationAlertActivity.class.getCanonicalName();
+
+    private GoogleMap mMap;
+    private GeofencingClient geofencingClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.location_alert_layout);
+
+        Toolbar tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        tb.setSubtitle("ГЕОЗОНЫ");
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.g_map);
+        mapFragment.getMapAsync(this);
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMinZoomPreference(15);
+
+        showCurrentLocationOnMap();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                addLocationAlert(latLng.latitude, latLng.longitude);
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void showCurrentLocationOnMap() {
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    private boolean isLocationAccessPermitted(){
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestLocationAccessPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                AppConstants.LOCATION_PERMANENT_REQUEST_CODE);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void addLocationAlert(double lat, double lng){
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else  {
+            String key = ""+lat+"-"+lng;
+            Geofence geofence = getGeofence(lat, lng, key);
+            geofencingClient.addGeofences(getGeofencingRequest(geofence),
+                    getGeofencePendingIntent())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Геозона была добавлена успешно",
+                                        Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Невозможно добавить геозону",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+    private void removeLocationAlert(){
+        if (isLocationAccessPermitted()) {
+            requestLocationAccessPermission();
+        } else {
+            geofencingClient.removeGeofences(getGeofencePendingIntent())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Геозона была успешно удалена",
+                                        Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(LocationAlertActivity.this,
+                                        "Невозможно удалить геозону",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case AppConstants.LOCATION_PERMANENT_REQUEST_CODE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showCurrentLocationOnMap();
+                    Toast.makeText(LocationAlertActivity.this,
+                            "Доступ к геоданным получен, Вы можете " +
+                                    "добавить или удалить геоточку",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+
+        }
+    }
+    private PendingIntent getGeofencePendingIntent() {
+        Intent intent = new Intent(this, LocationAlertIntentService.class);
+        return PendingIntent.getService(this, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private GeofencingRequest getGeofencingRequest(Geofence geofence) {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        builder.addGeofence(geofence);
+        return builder.build();
+    }
+
+    private Geofence getGeofence(double lat, double lang, String key) {
+        return new Geofence.Builder()
+                .setRequestId(key)
+                .setCircularRegion(lat, lang, AppConstants.GEOFENCE_RADIUS_IN_METERS)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay(10000)
+                .build();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.remove_loc_alert:
+                removeLocationAlert();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
